36a796aec8dbd0acff08d4b6ab572d0f9b089b2f
CarlosMansoCid <carlosmanso12@gmail.com>
2024-07-30 21:36:08 +0200

    added the basic css configuration

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import MainLayout from "../modules/common/layouts/main.layout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "", element: <App /> }],
+    children: [{ path: "", element: <p>Test</p> }],
   },
   {
     path: "*",
