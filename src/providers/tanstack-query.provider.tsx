16d5308f786e1c245a81b3a13c6be537c434f5e9
CarlosMansoCid <carlosmanso12@gmail.com>
2024-07-30 19:50:59 +0200

    configured tanstack query

---
+++ src/providers/tanstack-query.provider.tsx
@@ -0,0 +1,14 @@
+import { ReactNode, memo } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+interface ITanstackQueryProvider {
+  children: ReactNode;
+}
+const TanstackQueryProvider = ({ children }: ITanstackQueryProvider) => {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default memo(TanstackQueryProvider);
