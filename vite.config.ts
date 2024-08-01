89550615c93328c8dce080d353b460bdb1d35f46
CarlosMansoCid <carlosmanso12@gmail.com>
2024-08-02 01:43:44 +0200

    added vendors prefixes on build and optimized the bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer({})],
+  build: {
+    chunkSizeWarningLimit: 400000,
+    cssMinify: true,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
     },
   },
 });
