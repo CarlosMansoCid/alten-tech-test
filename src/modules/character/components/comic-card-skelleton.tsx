d40470747d67a79adc26a69a58da0af1c91630e7
CarlosMansoCid <carlosmanso12@gmail.com>
2024-08-02 23:17:12 +0200

    added comic fallback component

---
+++ src/modules/character/components/comic-card-skelleton.tsx
@@ -0,0 +1,27 @@
+import { memo } from "react";
+import Skelleton from "../../common/components/skelleton.component";
+import "../styles/comics-section.css";
+
+const ComicCardSkelleton = () => {
+  return (
+    <article className="comic_card__root">
+      <div className="comic_card__image_container ">
+        <Skelleton />
+      </div>
+      <div className="comic_card__info_container ">
+        <p className="comic_card__title">
+          <Skelleton style={{ width: "90%", height: "16px" }} />
+          <Skelleton
+            style={{ width: "80%", height: "16px", marginTop: "5px" }}
+          />
+        </p>
+        <p className="comic_card__date_tag">
+          <Skelleton
+            style={{ width: "20%", height: "8px", marginTop: "5px" }}
+          />
+        </p>
+      </div>
+    </article>
+  );
+};
+export default memo(ComicCardSkelleton);
